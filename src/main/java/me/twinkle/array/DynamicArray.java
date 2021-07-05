bd5779523ff93b60c553964fde611f8578e66939
twinklelahariya1 <twinklelahariya1@gmail.com>
2021-07-05 20:35:52 +0530

    Dynamic array implementation

---
+++ src/main/java/me/twinkle/array/DynamicArray.java
@@ -0,0 +1,36 @@
+package me.twinkle.array;
+
+import java.util.Arrays;
+
+public class DynamicArray {
+    public static void main(String[] args) {
+        new DynamicArray().run();
+    }
+
+    int index = 0;
+
+    int[] array = new int[2];
+
+    private void run() {
+
+        array = addElement(5, array);
+        array = addElement(6, array);
+        array = addElement(2, array);
+        array = addElement(4, array);
+        array = addElement(8, array);
+
+    }
+
+    private int[] addElement(int value, int[] array) {
+        if (index == array.length)
+            array = growArray(array, index);
+
+        array[index++] = value;
+        return array;
+    }
+
+    private int[] growArray(int[] array, int index) {
+        return Arrays.copyOf(array, index * 2);
+    }
+
+}
